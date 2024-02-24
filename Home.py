446708fc0b926d64d66d40a5afae862deebefd20
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-02-24 18:10:24 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,3 +1,27 @@
 import streamlit as st
 
 #GoogleMapsApiRouteKey=AIzaSyDBgNgcuFP8Swarp8gfR-PxLM8-Wm90774
+
+import googlemaps
+from datetime import datetime
+
+gmaps = googlemaps.Client(key='Add Your Key here')
+
+# Geocoding an address
+geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+
+# Look up an address with reverse geocoding
+reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+# Request directions via public transit
+now = datetime.now()
+directions_result = gmaps.directions("Sydney Town Hall",
+                                     "Parramatta, NSW",
+                                     mode="transit",
+                                     departure_time=now)
+
+# Validate an address with address validation
+addressvalidation_result =  gmaps.addressvalidation(['1600 Amphitheatre Pk'], 
+                                                    regionCode='US',
+                                                    locality='Mountain View', 
+                                                    enableUspsCass=True)
