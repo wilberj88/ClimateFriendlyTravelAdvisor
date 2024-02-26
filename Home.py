5e7dbd1105c517f0f8e56da05a765cc4fe26d194
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-02-26 19:30:51 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -18,44 +18,9 @@ st.title("🌎 Climate Friendly Travel Advisor📍")
 with st.sidebar:
     st.header("Plan your Trip")
     st.subheader("& discuss with me 🤖")
-    st.text_input(
+    origin = st.text_input(
         "Origin Point 👇")
-
-def new_york():
-
-    # Plot coordinates
-    coordinates = (40.75, -74)
-    _map = gmaps.figure(center=coordinates, zoom_level=12)
-
-    # Render map in Streamlit
-    snippet = embed.embed_snippet(views=_map)
-    html = embed.html_template.format(title="", snippet=snippet)
-    return components.html(html, height=500,width=500)
-
-new_york()
-
-
-
-api_key1 = st.secrets["GOOGLEMAPS_API_KEY"]
-
-gmaps = googlemaps.Client(key=api_key1)
-
-
-# Geocoding an address
-geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
-
-# Look up an address with reverse geocoding
-reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
-
-# Request directions via public transit
-now = datetime.now()
-directions_result = gmaps.directions("Sydney Town Hall",
-                                     "Parramatta, NSW",
-                                     mode="transit",
-                                     departure_time=now)
-
-# Validate an address with address validation
-addressvalidation_result =  gmaps.addressvalidation(['1600 Amphitheatre Pk'], 
-                                                    regionCode='US',
-                                                    locality='Mountain View', 
-                                                    enableUspsCass=True)
+    end = st.text_input(
+        "End Point 👇")
+    when = st.text_input(
+        "When 👇")
