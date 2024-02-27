8e601d3a1ef20c5fd7634e34e081511e1d47e395
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-02-27 02:34:53 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,229 +1,77 @@
 import streamlit as st
-import googlemaps
-import gmaps
-from datetime import date
-from ipywidgets import embed
-import streamlit.components.v1 as components
-#import crewai
-from crewai import Crew, Agent, Task
-from textwrap import dedent
-from langchain.llms import OpenAI
-from tools.browser_tools import BrowserTools
-from tools.calculator_tools import CalculatorTools
-from tools.search_tools import SearchTools
+import replicate
+import os
 
-# set page layout
-st.set_page_config(
-    page_title="Climate Friendly Travel Advisor",
-    page_icon="🌍",
-    layout="wide",
-    initial_sidebar_state="expanded",
-)
+# App title
+st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
 
-#functions
-class TripAgents():
-
-  def city_selection_agent(self):
-    return Agent(
-        role='City Selection Expert',
-        goal='Select the best city based on weather, season, and prices',
-        backstory=
-        'An expert in analyzing travel data to pick ideal destinations',
-        tools=[
-            SearchTools.search_internet,
-            BrowserTools.scrape_and_summarize_website,
-        ],
-        verbose=True)
-
-  def local_expert(self):
-    return Agent(
-        role='Local Expert at this city',
-        goal='Provide the BEST insights about the selected city',
-        backstory="""A knowledgeable local guide with extensive information
-        about the city, it's attractions and customs""",
-        tools=[
-            SearchTools.search_internet,
-            BrowserTools.scrape_and_summarize_website,
-        ],
-        verbose=True)
-
-  def travel_concierge(self):
-    return Agent(
-        role='Amazing Travel Concierge',
-        goal="""Create the most amazing travel itineraries with budget and 
-        packing suggestions for the city""",
-        backstory="""Specialist in travel planning and logistics with 
-        decades of experience""",
-        tools=[
-            SearchTools.search_internet,
-            BrowserTools.scrape_and_summarize_website,
-            CalculatorTools.calculate,
-        ],
-        verbose=True)
-    
-class TripTasks():
-
-  def identify_task(self, agent, origin, cities, interests, range):
-    return Task(description=dedent(f"""
-        Analyze and select the best city for the trip based 
-        on specific criteria such as weather patterns, seasonal
-        events, and travel costs. This task involves comparing
-        multiple cities, considering factors like current weather
-        conditions, upcoming cultural or seasonal events, and
-        overall travel expenses. 
-        
-        Your final answer must be a detailed
-        report on the chosen city, and everything you found out
-        about it, including the actual flight costs, weather 
-        forecast and attractions.
-        {self.__tip_section()}
-
-        Traveling from: {origin}
-        City Options: {cities}
-        Trip Date: {range}
-        Traveler Interests: {interests}
-      """),
-                agent=agent)
-
-  def gather_task(self, agent, origin, interests, range):
-    return Task(description=dedent(f"""
-        As a local expert on this city you must compile an 
-        in-depth guide for someone traveling there and wanting 
-        to have THE BEST trip ever!
-        Gather information about  key attractions, local customs,
-        special events, and daily activity recommendations.
-        Find the best spots to go to, the kind of place only a
-        local would know.
-        This guide should provide a thorough overview of what 
-        the city has to offer, including hidden gems, cultural
-        hotspots, must-visit landmarks, weather forecasts, and
-        high level costs.
-        
-        The final answer must be a comprehensive city guide, 
-        rich in cultural insights and practical tips, 
-        tailored to enhance the travel experience.
-        {self.__tip_section()}
-
-        Trip Date: {range}
-        Traveling from: {origin}
-        Traveler Interests: {interests}
-      """),
-                agent=agent)
-
-  def plan_task(self, agent, origin, interests, range):
-    return Task(description=dedent(f"""
-        Expand this guide into a a full 7-day travel 
-        itinerary with detailed per-day plans, including 
-        weather forecasts, places to eat, packing suggestions, 
-        and a budget breakdown.
-        
-        You MUST suggest actual places to visit, actual hotels 
-        to stay and actual restaurants to go to.
-        
-        This itinerary should cover all aspects of the trip, 
-        from arrival to departure, integrating the city guide
-        information with practical travel logistics.
-        
-        Your final answer MUST be a complete expanded travel plan,
-        formatted as markdown, encompassing a daily schedule,
-        anticipated weather conditions, recommended clothing and
-        items to pack, and a detailed budget, ensuring THE BEST
-        TRIP EVER, Be specific and give it a reason why you picked
-        # up each place, what make them special! {self.__tip_section()}
-
-        Trip Date: {range}
-        Traveling from: {origin}
-        Traveler Interests: {interests}
-      """),
-                agent=agent)
-
-  def __tip_section(self):
-    return "If you do your BEST WORK, I'll tip you $100!"
-
-
-class TripCrew:
-
-  def __init__(self, origin, cities, date_range, interests):
-    self.cities = cities
-    self.origin = origin
-    self.interests = interests
-    self.date_range = date_range
-
-  def run(self):
-    agents = TripAgents()
-    tasks = TripTasks()
-
-    city_selector_agent = agents.city_selection_agent()
-    local_expert_agent = agents.local_expert()
-    travel_concierge_agent = agents.travel_concierge()
-
-    identify_task = tasks.identify_task(
-      city_selector_agent,
-      self.origin,
-      self.cities,
-      self.interests,
-      self.date_range
-    )
-    gather_task = tasks.gather_task(
-      local_expert_agent,
-      self.origin,
-      self.interests,
-      self.date_range
-    )
-    plan_task = tasks.plan_task(
-      travel_concierge_agent, 
-      self.origin,
-      self.interests,
-      self.date_range
-    )
-
-    crew = Crew(
-      agents=[
-        city_selector_agent, local_expert_agent, travel_concierge_agent
-      ],
-      tasks=[identify_task, gather_task, plan_task],
-      verbose=True
-    )
-
-    result = crew.kickoff()
-    return result
-
-st.title("🌎 Climate Friendly Travel Advisor📍")
-
+# Replicate Credentials
 with st.sidebar:
-    st.header("Plan your Trip")
-    st.subheader("& discuss with me 🤖")
-    origin = st.text_input(
-        "Origin Point 👇")
-    end = st.text_input(
-        "End Point 👇")
-    when = st.time_input('Starts When?', value=None)
-    activate = st.button("Calculate CO2 Alternatives", type="primary")
-   
-if activate:
-    st.write("## Welcome to Trip Planner Crew")
-    st.write('-------------------------------')
-    location = st.text_input(
-    dedent("""
-      From where will you be traveling from?
-    """))
-    cities = st.text_input(
-    dedent("""
-      What are the cities options you are interested in visiting?
-    """))
-    date_range = st.text_input(
-    dedent("""
-      What is the date range you are interested in traveling?
-    """))
-    interests = st.text_input(
-    dedent("""
-      What are some of your high level interests and hobbies?
-    """))
-    
-    trip_crew = TripCrew(location, cities, date_range, interests)
-    result = trip_crew.run()
-    st.write("\n\n########################")
-    st.write("## Here is you Trip Plan")
-    st.write("########################\n")
-    st.write(result)
-  
+    st.title('🦙💬 Llama 2 Chatbot')
+    if 'REPLICATE_API_TOKEN' in st.secrets:
+        st.success('API key already provided!', icon='✅')
+        replicate_api = st.secrets['REPLICATE_API_TOKEN']
+    else:
+        replicate_api = st.text_input('Enter Replicate API token:', type='password')
+        if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
+            st.warning('Please enter your credentials!', icon='⚠️')
+        else:
+            st.success('Proceed to entering your prompt message!', icon='👉')
+    os.environ['REPLICATE_API_TOKEN'] = replicate_api
+
+    st.subheader('Models and parameters')
+    selected_model = st.sidebar.selectbox('Choose a Llama2 model', ['Llama2-7B', 'Llama2-13B'], key='selected_model')
+    if selected_model == 'Llama2-7B':
+        llm = 'a16z-infra/llama7b-v2-chat:4f0a4744c7295c024a1de15e1a63c880d3da035fa1f49bfd344fe076074c8eea'
+    elif selected_model == 'Llama2-13B':
+        llm = 'a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5'
+    temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
+    top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
+    max_length = st.sidebar.slider('max_length', min_value=32, max_value=128, value=120, step=8)
+    st.markdown('📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
+
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "Welcome to Trip Advisor! Let me know where do you plan traveling?"}]
+
+# Display or clear chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+def clear_chat_history():
+    st.session_state.messages = [{"role": "assistant", "content": "Welcome to Trip Advisor! Let me know where do you plan traveling?"}]
+st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
+
+# Function for generating LLaMA2 response. Refactored from https://github.com/a16z-infra/llama2-chatbot
+def generate_llama2_response(prompt_input):
+    string_dialogue = "You are a helpful assistant to give advices about travel with costs in time, speed and carbon emissions. You ask the 'User' to know how is the desire trip. You only respond once as 'Assistant'."
+    for dict_message in st.session_state.messages:
+        if dict_message["role"] == "user":
+            string_dialogue += "User: " + dict_message["content"] + "\n\n"
+        else:
+            string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
+    output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
+                           input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
+                                  "temperature":temperature, "top_p":top_p, "max_length":max_length, "repetition_penalty":1})
+    return output
+
+# User-provided prompt
+if prompt := st.chat_input(disabled=not replicate_api):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.write(prompt)
+
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = generate_llama2_response(prompt)
+            placeholder = st.empty()
+            full_response = ''
+            for item in response:
+                full_response += item
+                placeholder.markdown(full_response)
+            placeholder.markdown(full_response)
+    message = {"role": "assistant", "content": full_response}
+    st.session_state.messages.append(message)
