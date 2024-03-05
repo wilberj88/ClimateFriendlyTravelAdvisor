589f7225fa81dd4417e77eeaa106b1b5d81a9d66
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-03-05 23:34:28 +0100

    Update Prototyped2.py

--- pages/Prototyped2.py
+++ pages/Prototyped2.py
@@ -5,6 +5,33 @@ import os
 # App title
 st.set_page_config(page_title="🌎 Climate Friendly Travel Advisor")
 
+modes = ["Car", "Train", "Flying"]
+
+factor_emissions = {
+    "Train": 1,
+    "Car": 10,
+    "Flying": 20,
+}
+factor_time = {
+    "Train": 5,
+    "Car": 7,
+    "Flying": 2,
+}
+factor_speed = {
+    "Train": 10,
+    "Car": 7,
+    "Flying": 20,
+}
+factor_confort = {
+    "Train": 10,
+    "Car": 20,
+    "Flying": 30,
+}
+
+
+def emissions():
+    
+
 def turn_on_bot():
     pass
 
@@ -14,4 +41,5 @@ with st.sidebar:
     st.subheader ('Empowered by Langchain-OpenAI-ChatGPT-API')
     origin = st.text_input('Enter origin:')
     destiny = st.text_input('Enter destiny:')
+    prefer_mode = st.select_box(modes)
     activate_advisor = st.button('🤖 Ask Advisor!', on_click=turn_on_bot)
