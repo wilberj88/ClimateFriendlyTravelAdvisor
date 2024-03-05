ca6f3f764d717b10042fabeb4da4799d478c5236
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-03-05 23:16:14 +0100

    Update Prototyped2.py

--- pages/Prototyped2.py
+++ pages/Prototyped2.py
@@ -4,3 +4,8 @@ import os
 
 # App title
 st.set_page_config(page_title="🌎 Climate Friendly Travel Advisor")
+
+with st.sidebar:
+    st.title('🌎 Climate Friendly Travel Advisor')
+    st.header('Prototyped 2')
+    st.subheader ('Empowered by OpenAI-ChatGPT-API')
