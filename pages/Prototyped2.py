463e5eb9764e36a5f829710ae56dbb3c4fc41411
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-03-05 23:21:04 +0100

    Update Prototyped2.py

--- pages/Prototyped2.py
+++ pages/Prototyped2.py
@@ -5,7 +5,13 @@ import os
 # App title
 st.set_page_config(page_title="🌎 Climate Friendly Travel Advisor")
 
+def turn_on_bot:
+    pass
+
 with st.sidebar:
     st.title('🌎 Climate Friendly Travel Advisor')
     st.header('Prototyped 2')
     st.subheader ('Empowered by OpenAI-ChatGPT-API')
+    origin = st.text_input('Enter origin:')
+    destiny = st.text_input('Enter destiny:')
+    activate_advisor = st.button.('Clear Chat History', on_click=turn_on_bot)
